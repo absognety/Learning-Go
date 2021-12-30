a3250159c46dab1b5ae86720895e0973d61d2a51
Vikas Chitturi <csubramanya@athenahealth.com>
2021-12-30 20:55:45 +0530

    Determine if year is a leap year or not using logical operators

---
+++ leapyear.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func determineLeapYear() {
+	var year = 1900
+	if ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0) {
+		fmt.Printf("Given year %v is a leap year", year)
+	} else {
+		fmt.Printf("Given year %v is not a leap year", year)
+	}
+}
