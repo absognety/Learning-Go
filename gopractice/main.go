bb60df13630550997043f7c54193011fd3152e02
Vikas Chitturi <csubramanya@athenahealth.com>
2024-07-14 14:43:09 +0530

    Convert string to floating value

--- gopractice/main.go
+++ gopractice/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,4 +14,14 @@ func main() {
 	// ReadString will block until the delimiter is entered
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered: ", input)
+
+
+	fmt.Print("Enter a number: ")
+	input, _ = reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value entered is: ", aFloat)
+	}
 }
